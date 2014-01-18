2a82dd1e5e0c6e61f8d56982a78169626f7fb45e
Wesley Reardan <reardan@gmail.com>
2014-01-18 14:30:10 -0500

    Changed port for heroku

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var app = express();
+
+var port = process.env.PORT || 3000;
+
 app.configure(function () {
     app.use(
         "/", //the URL throught which you want to access to you static content
         express.static(__dirname) //where your static content is located in your filesystem
     );
 });
-app.listen(80); //the port you want to use
+app.listen(port); //the port you want to use
